26a12f4a1b4c0bc51fed1326d01c866ff596c03c
aartaka <aartaka@protonmail.com>
2020-09-02 12:57:15 +0500

    recon.scm: fix indentation

--- chur/recon.scm
+++ chur/recon.scm
@@ -64,33 +64,33 @@ For more information about CT logs, check www.certificate-transparency.org and c
 
 (define-public perl-rpc-xml
   (package
-  (name "perl-rpc-xml")
-  (version "0.80")
-  (source
-    (origin
-      (method url-fetch)
-      (uri (string-append
+    (name "perl-rpc-xml")
+    (version "0.80")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
              "mirror://cpan/authors/id/R/RJ/RJRAY/RPC-XML-"
              version
              ".tar.gz"))
-      (sha256
+       (sha256
         (base32
-          "1xvy9hs7bqsjnk0663kf7zk2qjg0pzv96n6z2wlc2w5bgal7q3ga"))))
-  (build-system perl-build-system)
-  (propagated-inputs
-    `(("perl-datetime" ,perl-datetime)
-      ("perl-datetime-format-iso8601"
-       ,perl-datetime-format-iso8601)
-      ("perl-libwww" ,perl-libwww)
-      ("perl-xml-libxml" ,perl-xml-libxml)
-      ("perl-xml-parser" ,perl-xml-parser)))
-  (home-page
-    "https://metacpan.org/release/RPC-XML")
-  (synopsis
-    "Data, client and server classes for XML-RPC")
-  (description "This package is an implementation of the XML-RPC standard in Perl.
+         "1xvy9hs7bqsjnk0663kf7zk2qjg0pzv96n6z2wlc2w5bgal7q3ga"))))
+    (build-system perl-build-system)
+    (propagated-inputs
+     `(("perl-datetime" ,perl-datetime)
+       ("perl-datetime-format-iso8601"
+        ,perl-datetime-format-iso8601)
+       ("perl-libwww" ,perl-libwww)
+       ("perl-xml-libxml" ,perl-xml-libxml)
+       ("perl-xml-parser" ,perl-xml-parser)))
+    (home-page
+     "https://metacpan.org/release/RPC-XML")
+    (synopsis
+     "Data, client and server classes for XML-RPC")
+    (description "This package is an implementation of the XML-RPC standard in Perl.
 The goal is to provide a client, a stand-alone server and an Apache/mod_perl content-handler class.")
-  (license license:perl-license)))
+    (license license:perl-license)))
 
 (define-public nikto
   (let ((commit "d8eb05496f69c0dc703d19dc012d2547d0467705")
@@ -135,51 +135,51 @@ exec ~a ~a/program/nikto.pl \"$@\"" bash perl out)))
 
 (define-public python-xlsxwriter
   (package
-   (name "python-xlsxwriter")
-   (version "1.2.8")
-   (source
-    (origin
-     (method url-fetch)
-     (uri (pypi-uri "XlsxWriter" version))
-     (sha256
-      (base32
-       "0sv553dj5h3qxbn8xfaqxr3bx2lglp85cxlcsnf3mzqnmf41k3j8"))))
-   (build-system python-build-system)
-   (home-page "https://github.com/jmcnamara/XlsxWriter")
-   (synopsis "A Python module for creating Excel XLSX files.")
-   (description "A Python module for creating Excel XLSX files.")
-   (license license:bsd-3)))
+    (name "python-xlsxwriter")
+    (version "1.2.8")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "XlsxWriter" version))
+       (sha256
+        (base32
+         "0sv553dj5h3qxbn8xfaqxr3bx2lglp85cxlcsnf3mzqnmf41k3j8"))))
+    (build-system python-build-system)
+    (home-page "https://github.com/jmcnamara/XlsxWriter")
+    (synopsis "A Python module for creating Excel XLSX files.")
+    (description "A Python module for creating Excel XLSX files.")
+    (license license:bsd-3)))
 
 (define-public python-shodan
   (package
-   (name "python-shodan")
-   (version "1.23.0")
-   (source
-    (origin
-     (method url-fetch)
-     (uri (pypi-uri "shodan" version))
-     (sha256
-      (base32
-       "16rkbhdj7al7p8s1pfsjx9agxpvisbvyvcd04rm1kigpz87p9c1i"))))
-   (build-system python-build-system)
-   (arguments
-    `(#:phases (modify-phases %standard-phases (delete 'check))))
-   (propagated-inputs
-    `(("python-click" ,python-click)
-      ("python-click-plugins" ,python-click-plugins)
-      ("python-colorama" ,python-colorama)
-      ("python-requests" ,python-requests)
-      ("python-xlsxwriter" ,python-xlsxwriter)
-      ("python-pytest" ,python-pytest)
-      ("python-aiodns" ,python-aiodns)
-      ("python-beautifulsoup4" ,python-beautifulsoup4)))
-   (home-page
-    "http://github.com/achillean/shodan-python/tree/master")
-   (synopsis
-    "Python library and command-line utility for Shodan (https://developer.shodan.io)")
-   (description
-    "Python library and command-line utility for Shodan (https://developer.shodan.io)")
-   (license #f)))
+    (name "python-shodan")
+    (version "1.23.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "shodan" version))
+       (sha256
+        (base32
+         "16rkbhdj7al7p8s1pfsjx9agxpvisbvyvcd04rm1kigpz87p9c1i"))))
+    (build-system python-build-system)
+    (arguments
+     `(#:phases (modify-phases %standard-phases (delete 'check))))
+    (propagated-inputs
+     `(("python-click" ,python-click)
+       ("python-click-plugins" ,python-click-plugins)
+       ("python-colorama" ,python-colorama)
+       ("python-requests" ,python-requests)
+       ("python-xlsxwriter" ,python-xlsxwriter)
+       ("python-pytest" ,python-pytest)
+       ("python-aiodns" ,python-aiodns)
+       ("python-beautifulsoup4" ,python-beautifulsoup4)))
+    (home-page
+     "http://github.com/achillean/shodan-python/tree/master")
+    (synopsis
+     "Python library and command-line utility for Shodan (https://developer.shodan.io)")
+    (description
+     "Python library and command-line utility for Shodan (https://developer.shodan.io)")
+    (license #f)))
 
 (define-public theharvester
   (let ((commit "ceb4c3b1527aa05eb92a028036a4b73d78eb389b")
